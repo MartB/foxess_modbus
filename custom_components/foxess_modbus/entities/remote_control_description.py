3fd6b6666a6dc5a8b1345a7b0ed66841d53efe79
Martin Böh <contact@martb.dev>
2025-10-29 22:05:59 +0000

    remote_control: fix invalid registers, add h180 registers

--- custom_components/foxess_modbus/entities/remote_control_description.py
+++ custom_components/foxess_modbus/entities/remote_control_description.py
@@ -100,16 +100,30 @@ REMOTE_CONTROL_DESCRIPTION = ModbusRemoteControlFactory(
                 work_mode=41000,
                 work_mode_map=_NORMAL_WORK_MODE_MAP,
                 max_soc=41010,
-                invbatpower=[31022],
+                invbatpower=[31036],
                 battery_soc=[31038],
                 pwr_limit_bat_up=None,
                 pv_voltages=[31000, 31003],
             ),
-            models=Inv.H3_SET & ~Inv.KUARA_H3 & ~Inv.AIO_H3_101 & ~Inv.AIO_H3_PRE101,
+            models=Inv.H3_PRE180 & ~Inv.KUARA_H3 & ~Inv.AIO_H3_101 & ~Inv.AIO_H3_PRE101,
         ),
         RemoteControlAddressSpec(
-            # The H3 doesn't support anything above 44005, and the active/reactive power regisers are 2 values
-            # The Kuara H3 doesn't support this, see https://github.com/nathanmarlor/foxess_modbus/issues/532
+            # H3 after 180 supports pwr_limit_bat_up
+            holding=ModbusRemoteControlAddressConfig(
+                remote_enable=44000,
+                timeout_set=44001,
+                active_power=[44003, 44002],
+                work_mode=41000,
+                work_mode_map=_NORMAL_WORK_MODE_MAP,
+                max_soc=41010,
+                invbatpower=[31036],
+                battery_soc=[31038],
+                pwr_limit_bat_up=[44012],
+                pv_voltages=[31000, 31003],
+            ),
+            models=Inv.H3_180,
+        ),
+        RemoteControlAddressSpec(
             holding=ModbusRemoteControlAddressConfig(
                 remote_enable=46001,
                 timeout_set=46002,
@@ -123,7 +137,7 @@ REMOTE_CONTROL_DESCRIPTION = ModbusRemoteControlFactory(
                 max_soc=46610,
                 invbatpower=[39238, 39237],
                 battery_soc=[37612, 38310],
-                pwr_limit_bat_up=[46019, 46018],
+                pwr_limit_bat_up=[46021, 46020],
                 pv_voltages=[39070, 39072, 39074, 39076, 39078, 39080],
             ),
             models=Inv.H3_PRO_SET | Inv.H3_SMART,
